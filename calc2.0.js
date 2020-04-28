fe6b5e8dda2eadf84f7017642a40ad5b88e299a9
Deputat Albert <nikitaizmewstev@yandex.ru>
2020-04-28 23:40:04 +0700

    first commit

---
+++ calc2.0.js
@@ -0,0 +1,64 @@
+function insert (num){
+	/*let test;
+	test = document.getElementById('n1').textContent;
+	
+	document.getElementById('output').innerHTML = document.getElementById('output').innerHTML + num;
+*/
+
+document.form.textview.value = document.form.textview.value + num;
+
+}
+
+
+function equal() {
+	let ravno;
+	ravno = document.form.textview.value;
+	
+	if (eval(ravno) == 'Infinity'){
+	document.form.textview.value = "ERROR";
+
+} else {
+	
+	document.form.textview.value = eval(ravno);
+		}
+
+}
+
+function reset(){
+	document.form.textview.value = '';
+}
+
+
+
+function change_color() {
+var dlina = document.getElementsByClassName('number').length;
+
+document.querySelector('.main__container').classList.toggle('mcdark');
+document.querySelector('.form__output').classList.toggle('fodark');
+document.querySelector('.output').classList.toggle('outputdark');
+
+
+let x = document.querySelectorAll('.number');
+
+for(let i = 0; i < x.length; i++ ) {
+	x[i].classList.toggle('numberdark');
+}
+
+document.querySelector('.number__eq').classList.toggle('number__eq_dark');
+document.querySelector('.btn__change').classList.toggle('btn__change_dark');
+document.querySelector('.bodyy').classList.toggle('body_dark');
+
+/*document.querySelectorAll('.number').classList.toggle('numberdark');*/
+
+
+document.querySelector('.main__container').classList.toggle('norm');
+document.querySelector('.form__output').classList.toggle('norm');
+document.querySelector('.output').classList.toggle('norm');
+
+
+
+console.log("WORK");
+
+
+
+}
